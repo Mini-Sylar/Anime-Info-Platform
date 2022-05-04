a03ca98ccb054fb0b03757c9c3c6a65629d1f209
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-05-04 13:16:01 +0000

    Added animation effects

--- index.js
+++ index.js
@@ -18,41 +18,46 @@ const form = document.querySelector("#search-form");
 let get_genre;
 let get_ID;
 function randomIntFromInterval(min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function Replace(data) {
-  // console.log(data);
-  main_data = data.data.Media;
-  title.innerHTML = data.data.Media.title.english
-    ? main_data.title.english
-    : main_data.title.romaji;
-  title.classList.add("addtransition");
-  description.innerHTML = data.data.Media.description;
-  // description.querySelectorAll("br").forEach((element) => {
-  //   element.remove()
-  // });
-  release_year.innerHTML = main_data.seasonYear;
-  genre.innerHTML = main_data.genres.join(" / ");
-  get_genre = genre.textContent;
-  episode_count.innerHTML = `${main_data.episodes} episodes`;
-  rating.innerHTML = `${main_data.averageScore / 10}/10`;
-  get_ID = main_data.id;
-  more_info.addEventListener("click", function () {
-    window.open(`https://anilist.co/anime/${get_ID}`, "_blank");
-  });
+    main_data = data.data.Media;
+    //   Render title Here
+    title.innerHTML = data.data.Media.title.english
+        ? ` <h1 class="anime-title style-1">${main_data.title.english}</h1>`
+        : ` <h1 class="anime-title style-1">${main_data.title.romaji}</h1>`;
+    //   Render Description Here
+    description.innerHTML = `<p class="anime-synopsis style-1">${data.data.Media.description}</p>`;
+    //   Render Release Year
+    release_year.innerHTML = ` <span class="year">${main_data.seasonYear}</span>`;
+    //   Render Genre
+    genre.innerHTML = `<span class="genre">${main_data.genres.join(
+        " / "
+    )}</span>`;
+    get_genre = genre.textContent;
+    //   Render Episode Count
+    episode_count.innerHTML = ` <span class="episode-count">${main_data.episodes} episodes</span>`;
+    //   Render Rating
+    rating.innerHTML = `<span class="rating">${main_data.averageScore / 10
+        }/10</span>`;
+    get_ID = main_data.id;
+    //   Navigate to Anilist
+    more_info.addEventListener("click", function () {
+        window.open(`https://anilist.co/anime/${get_ID}`, "_blank");
+    });
 }
 
 let headersList = {
-  Accept: "*/*",
-  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-  "Content-Type": "application/json",
+    Accept: "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Content-Type": "application/json",
 };
 
 function callBody(setID = 140960) {
-  let gqlBody = {
-    query: `query ($id: Int) {
+    let gqlBody = {
+        query: `query ($id: Int) {
   Media (id: $id, type: ANIME) { 
     id
     title {
@@ -70,29 +75,29 @@ function callBody(setID = 140960) {
   averageScore
   }
 }`,
-    variables: { id: setID },
-  };
+        variables: { id: setID },
+    };
 
-  let bodyContent = JSON.stringify(gqlBody);
+    let bodyContent = JSON.stringify(gqlBody);
 
-  fetch("https://graphql.anilist.co/?id=15125", {
-    method: "POST",
-    body: bodyContent,
-    headers: headersList,
-  })
-    .then(function (response) {
-      return response.json();
+    fetch("https://graphql.anilist.co/?id=15125", {
+        method: "POST",
+        body: bodyContent,
+        headers: headersList,
     })
-    .then(function (data) {
-      Replace(data);
-      get_genre ? get_genre.split("/").join(",") : "Action";
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            Replace(data);
+            get_genre ? get_genre.split("/").join(",") : "Action";
+        });
 }
 
 //   For Cards
 let card_main;
 let gqlBody_Cards = {
-  query: `query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+    query: `query ($id: Int, $page: Int, $perPage: Int, $search: String) {
   Page (page: $page, perPage: $perPage) {
     pageInfo {
       perPage
@@ -112,77 +117,77 @@ let gqlBody_Cards = {
   }
 }
 `,
-  variables: {
-    search: get_genre ? get_genre.split("/").join(",") : "Action",
-    page: randomIntFromInterval(1, 200),
-    perPage: 5,
-  },
+    variables: {
+        search: get_genre ? get_genre.split("/").join(",") : "Action",
+        page: randomIntFromInterval(1, 200),
+        perPage: 5,
+    },
 };
 
 let cardContent = JSON.stringify(gqlBody_Cards);
 
 fetch("https://graphql.anilist.co/", {
-  method: "POST",
-  body: cardContent,
-  headers: headersList,
+    method: "POST",
+    body: cardContent,
+    headers: headersList,
 })
-  .then(function (response) {
-    return (card_main = response.json());
-  })
-  .then(function (card_data) {
-    console.log(card_data);
-    console.log(
-      card_data.data.Page.media[2].title.english
-        ? card_data.data.Page.media[2].title.english
-        : card_data.data.Page.media[2].title.romaji
-    );
-    const find_card = card_data.data.Page.media;
-    anime_cards.forEach((currentElement, index) => {
-      currentElement.innerHTML = find_card[index].title.english
-        ? find_card[index].title.english
-        : find_card[index].title.romaji;
-      currentElement.style = `
+    .then(function (response) {
+        return (card_main = response.json());
+    })
+    .then(function (card_data) {
+        console.log(card_data);
+        console.log(
+            card_data.data.Page.media[2].title.english
+                ? card_data.data.Page.media[2].title.english
+                : card_data.data.Page.media[2].title.romaji
+        );
+        const find_card = card_data.data.Page.media;
+        anime_cards.forEach((currentElement, index) => {
+            currentElement.innerHTML = find_card[index].title.english
+                ? find_card[index].title.english
+                : find_card[index].title.romaji;
+            currentElement.style = `
         background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0,0.6)), url(${find_card[index].coverImage.extraLarge});
         background-repeat: no-repeat;
         background-size: cover;
       `;
-      currentElement.addEventListener("mouseenter", function () {
-        currentElement.style = `
+            currentElement.addEventListener("mouseenter", function () {
+                currentElement.style = `
         background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0,0.1)), url(${find_card[index].coverImage.extraLarge});
         background-repeat: no-repeat;
         background-size: cover;
         transition: all 1s ease;
         `;
-      });
-      currentElement.addEventListener("mouseleave", function () {
-        currentElement.style = `
+            });
+            currentElement.addEventListener("mouseleave", function () {
+                currentElement.style = `
         background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0,0.6)), url(${find_card[index].coverImage.extraLarge});
         background-repeat: no-repeat;
         background-size: cover;
         transition: all 1s ease;
         `;
-      });
+            });
 
-      //    Onclick Replace Everything
-      currentElement.addEventListener("click", function () {
-        let getThatID = find_card[index].id;
-        console.log(getThatID);
-        // MYQUERY HERE
-        callBody(getThatID);
-      });
+            //    Onclick Replace Everything
+            currentElement.addEventListener("click", function () {
+                let getThatID = find_card[index].id;
+                console.log(getThatID);
+                // MYQUERY HERE
+                callBody(getThatID);
+            });
+        });
     });
-  });
 
 // ============== Search Section =====================
 function SearchAnime(searchQuery) {
-  let headersList = {
-    Accept: "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    "Content-Type": "application/json",
-  };
+    let headersList = {
+        Accept: "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json",
+    };
 
-  let gqlBody = {
-    query: `query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+    let gqlBody = {
+        query: `query ($id: Int, $page: Int, $perPage: Int, $search: String) {
   Page (page: $page, perPage: $perPage) {
     pageInfo {
       total
@@ -200,30 +205,30 @@ function SearchAnime(searchQuery) {
     }
   }
 }`,
-    variables: { search: searchQuery, page: 1, perPage: 1 },
-  };
+        variables: { search: searchQuery, page: 1, perPage: 1 },
+    };
 
-  let bodyContent = JSON.stringify(gqlBody);
+    let bodyContent = JSON.stringify(gqlBody);
 
-  fetch("https://graphql.anilist.co/", {
-    method: "POST",
-    body: bodyContent,
-    headers: headersList,
-  })
-    .then(function (response) {
-      return response.json();
+    fetch("https://graphql.anilist.co/", {
+        method: "POST",
+        body: bodyContent,
+        headers: headersList,
     })
-    .then(function (search_data) {
-      console.log(search_data);
-      let thisID = search_data.data.Page.media[0].id;
-      callBody(thisID);
-    });
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (search_data) {
+            console.log(search_data);
+            let thisID = search_data.data.Page.media[0].id;
+            callBody(thisID);
+        });
 }
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  SearchAnime(search_value.value);
-  form.reset();
+    e.preventDefault();
+    SearchAnime(search_value.value);
+    form.reset();
 });
 
 // console.log(localStorage["mySearchQuery"]);
