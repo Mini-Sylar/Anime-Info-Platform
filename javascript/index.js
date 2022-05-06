a4af51740ef0391ecf37dff6395929d7c895e725
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-05-06 17:15:18 +0000

    test

--- javascript/index.js
+++ javascript/index.js
@@ -135,7 +135,7 @@ function Replace(data) {
       : `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(${main_data.bannerImage})`;
 
   body.style.backgroundSize = "cover";
-  body.style.backgroundPosition = "fixed";
+  // body.style.backgroundPosition = "center center";
   body.classList.add("addtransition");
   mobile_nav.style.backgroundColor = shadeColor(get_Color, -60);
 
