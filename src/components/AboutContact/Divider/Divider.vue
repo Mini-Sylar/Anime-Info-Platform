c6a49b7d07e1a32cdc666827e5828e21fde51eec
Mini-Sylar <minisylar3@gmail.com>
2023-04-17 00:10:17 +0000

    fix divider arrow

--- src/components/AboutContact/Divider/Divider.vue
+++ src/components/AboutContact/Divider/Divider.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="top-line dividers"></div>
         <div class="icon arrow-icon-divider" @click=changeWidth>
-            <svg :class="[initiaChangeWidth == true ? 'rotate-right uses-dynamic arrow-right' : 'rotate-left uses-dynamic arrow-right']"
+            <svg :class="[$route.path == '/contact' ? 'rotate-right uses-dynamic arrow-right' : 'rotate-left uses-dynamic arrow-right']"
                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path
                     d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
@@ -14,14 +14,19 @@
 <script setup>
 import { useAnimeData } from '../../../stores/anime_data';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 const animeData = useAnimeData();
 
 const initiaChangeWidth = ref(animeData.toggleAbout);
 
+const router = useRouter();
 const changeWidth = () => {
-    initiaChangeWidth.value = !initiaChangeWidth.value;
-    animeData.reduceWidth(initiaChangeWidth.value);
-
+    console.log(router.currentRoute.value.path)
+    if (router.currentRoute.value.path == '/contact') {
+        router.push('/about')
+        return
+    }
+    router.push('/contact')
 }
 </script>
 <style scoped>
