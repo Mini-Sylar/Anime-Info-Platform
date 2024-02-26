5b38c927527c0869b676e4079b3504844d1fc95c
Mini-Sylar <minisylar3@gmail.com>
2024-02-26 15:45:47 +0000

    Add VitePWA plugin configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,32 @@ export default defineConfig({
   build: {
     target: "esnext",
   },
-  plugins: [vue(),VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        start_url: '/auth/login',
+        name: 'Anime Info Platform 2.0', 
+        short_name: 'Anime Info',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        theme_color: '#f6f6f6',
+        background_color: '#f6f6f6',
+        display: 'standalone',
+      }
+    })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
